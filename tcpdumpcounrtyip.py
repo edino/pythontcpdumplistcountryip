9b2d7ceb46c5a43ea297ace4e9ce06d0acaa2c91
Edino de Souza <eddypqd@gmail.com>
2023-11-14 17:08:08 -0800

    Update tcpdumpcounrtyip.py

--- tcpdumpcounrtyip.py
+++ tcpdumpcounrtyip.py
@@ -1,9 +1,11 @@
 import subprocess
-import requests
+import re
+import json
+import socket
 
 def get_unique_ips():
     # Run tcpdump to capture packets and extract unique IP addresses
-    tcpdump_cmd = "sudo tcpdump -veni PortB -s0 -Q in -c 1000 'not (host 54.228.158.66)' | awk '/IP/{print $3}' | sort -u"
+    tcpdump_cmd = "sudo tcpdump -veni PortB -s0 -Q in -c 10 'not (host 54.228.158.66)' | awk '/IP/{print $3}' | sort -u"
     process = subprocess.Popen(tcpdump_cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
     output, error = process.communicate()
     if error:
@@ -13,14 +15,31 @@ def get_unique_ips():
 
 def get_country(ip):
     try:
-        response = requests.get(f"http://ip-api.com/json/{ip}")
-        if response.status_code == 200:
-            data = response.json()
-            country_name = data.get("country", "Unknown")
-            return country_name
-        else:
-            return "Unknown"
-    except requests.RequestException as e:
+        # Create a socket object
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            # Connect to the API endpoint
+            s.connect(("ip-api.com", 80))
+
+            # Send GET request
+            request = f"GET /json/{ip} HTTP/1.1\r\nHost: ip-api.com\r\nConnection: close\r\n\r\n"
+            s.send(request.encode())
+
+            # Receive response data
+            response = b""
+            while True:
+                data = s.recv(4096)
+                if not data:
+                    break
+                response += data
+
+            # Extract country from response
+            country_data = re.search(b'"country":"([^"]+)"', response)
+            if country_data:
+                country_name = country_data.group(1).decode()
+                return country_name
+            else:
+                return "Unknown"
+    except Exception as e:
         print(f"Error fetching data for IP {ip}: {e}")
         return "Unknown"
 
