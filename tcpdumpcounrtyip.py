2f932d4d3da3748497c045f8870c25109ddf8ce5
Edino de Souza <eddypqd@gmail.com>
2023-11-14 17:05:00 -0800

    Update tcpdumpcounrtyip.py

--- tcpdumpcounrtyip.py
+++ tcpdumpcounrtyip.py
@@ -1,35 +1,35 @@
 import subprocess
-from collections import defaultdict
+import requests
 
 def get_unique_ips():
-    # Run tcpdump and collect IP addresses
-    tcpdump_command = ['sudo', 'tcpdump', '-veni', '-Q', 'in' , '-c', '10', '-s0']
-    tcpdump_output = subprocess.run(tcpdump_command, capture_output=True, text=True)
-    
-    ip_addresses = set()
-    lines = tcpdump_output.stdout.split('\n')
-    
-    for line in lines:
-        if 'IP' in line:
-            ip = line.split()[2].split('.')[0]
-            ip_addresses.add(ip)
-    
-    return list(ip_addresses)
+    # Run tcpdump to capture packets and extract unique IP addresses
+    tcpdump_cmd = "sudo tcpdump -veni PortB -c 1000 -l 'not (host 54.228.158.66)' | awk '/IP/{print $3}' | sort -u"
+    process = subprocess.Popen(tcpdump_cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    output, error = process.communicate()
+    if error:
+        print(f"Error: {error}")
+        return []
+    return output.splitlines()
 
-def get_countries(ip_addresses):
-    # Get countries for each IP address
-    countries = defaultdict(str)
-    for ip in ip_addresses:
-        command = ['geoiplookup', ip]
-        result = subprocess.run(command, capture_output=True, text=True)
-        country = result.stdout.split()[-1]
-        countries[ip] = country
+def get_country(ip):
+    try:
+        response = requests.get(f"http://ip-api.com/json/{ip}")
+        if response.status_code == 200:
+            data = response.json()
+            country_name = data.get("country", "Unknown")
+            return country_name
+        else:
+            return "Unknown"
+    except requests.RequestException as e:
+        print(f"Error fetching data for IP {ip}: {e}")
+        return "Unknown"
 
-    return countries
+def main():
+    unique_ips = get_unique_ips()
+    if unique_ips:
+        for ip in unique_ips:
+            country = get_country(ip)
+            print(f"IP Address: {ip} | Country: {country}")
 
 if __name__ == "__main__":
-    unique_ips = get_unique_ips()
-    ip_countries = get_countries(unique_ips)
-    
-    for ip, country in ip_countries.items():
-        print("IP: %s | Country: %s" % (ip, country))
+    main()
