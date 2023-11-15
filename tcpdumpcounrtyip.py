056a3d7571e98e174d2e79d983d24f877c0c88f0
Edino de Souza <eddypqd@gmail.com>
2023-11-14 16:51:42 -0800

    Update tcpdumpcounrtyip.py

--- tcpdumpcounrtyip.py
+++ tcpdumpcounrtyip.py
@@ -3,15 +3,15 @@ from collections import defaultdict
 
 def get_unique_ips():
     # Run tcpdump and collect IP addresses
-    command = ['sudo', 'tcpdump', '-n', '-c', '1000']
-    tcpdump_output = subprocess.run(command, capture_output=True, text=True)
-    
+    tcpdump_command = ['sudo', 'tcpdump', '-n', '-c', '1000']
+    tcpdump_output = subprocess.run(tcpdump_command, capture_output=True, text=True)
+
     ip_addresses = set()
     lines = tcpdump_output.stdout.split('\n')
     
     for line in lines:
         if 'IP' in line:
-            ip = line.split()[2]
+            ip = line.split()[2].split('.')[0]
             ip_addresses.add(ip)
     
     return list(ip_addresses)
@@ -21,7 +21,7 @@ def get_countries(ip_addresses):
     countries = defaultdict(str)
     for ip in ip_addresses:
         command = ['geoiplookup', ip]
-        result = subprocess.run(command, capture_output=True, text=True)
+        result = subprocess.run(command, capture_output=True, text=True, shell=False)
         country = result.stdout.split()[-1]
         countries[ip] = country
 
